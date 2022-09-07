110861648e718882f4b74ebcca58906651cccc60
camisoriahz <camilasoriahz@gmail.com>
2022-09-07 16:23:01 -0300

    login func

--- afip_script.py
+++ afip_script.py
@@ -4,7 +4,7 @@
 import bs4
 import requests
 import re
-import selenium
+from selenium import webdriver
 from datetime import date
 import openpyxl
 from openpyxl.styles import Font
@@ -12,12 +12,21 @@ from openpyxl.styles import Font
 # def extraer_datos():
 # del excel
 
-def login(cuit, clave_fiscal):
-# entrar a la pag
+def login(browser, cuit, clave_fiscal):
+  try:
+    browser.get('https://auth.afip.gob.ar/contribuyente_/login.xhtml')
+    browser.find_element_by_id('F1:username').send_keys(cuit)
+    browser.find_element_by_id('F1:btnSiguiente').click()
+    browser.find_element_by_id('F1:password').send_keys(clave_fiscal)
+    browser.find_element_by_id('F1:btnIngresar').click()
+  except:
+    print('Error en login')
 
-def siper():
+#def siper():
 # sacar el riesgo
 
 if __name__ == '__main__':
   cuit = input('Ingrese el CUIT: ')
   clave_fiscal = input('Ingrese la clave fiscal: ')
+  browser = webdriver.Chrome()
+  login(browser, cuit, clave_fiscal)
