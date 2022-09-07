c147add2de7e39522cd4bda6229a210bbe6d501d
camisoriahz <camilasoriahz@gmail.com>
2022-09-07 11:36:50 -0300

    afip

---
+++ afip_script.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import bs4
+import requests
+import re
+import selenium
+from datetime import date
+import openpyxl
+from openpyxl.styles import Font
+
+def extraer_datos():
+# del excel
+
+def login():
+# entrar a la pag
+
+def siper():
+# sacar el riesgo
+
+if __name__ == '__main__':
+  cuit = 20243384525 #input('Ingrese el CUIT: ')
+  clave_fiscal = Almamia2021 #input('Ingrese la clave fiscal: ')
+  
