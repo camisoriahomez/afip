df7292351371216a8b7f572cfbbff56f26bd9670
GastonOrphant <105060758+GastonOrphant@users.noreply.github.com>
2022-10-07 15:05:23 -0300

    Update afip_script.py

--- afip_script.py
+++ afip_script.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import bs4
-import requests
-import re
-import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import date
-import openpyxl
-from openpyxl.styles import Font
+import gspread
 import time
 
 #Depurar errores de selenium en windows
 options = webdriver.ChromeOptions() 
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-def extraer_cuil(i, sheet_obj,path):
-  #del excel
-  cell_obj = sheet_obj.cell(row = i, column = 2)
+def extraer_cuil(i, worksheet):
+  cell_obj = worksheet.cell(i, 2)
   return cell_obj.value
 
-def extraer_clave(i, sheet_obj, path):
-  # del excel
-  cell_obj = sheet_obj.cell(row = i, column = 3)
+def extraer_clave(i, worksheet):
+  cell_obj = worksheet.cell(i, 3)
   return cell_obj.value
 
 def login(browser, cuit, clave_fiscal):
@@ -66,17 +59,41 @@ def deuda(browser, cuit, clave_fiscal):
     # login de nuevo
     browser.get('https://ctacte.cloud.afip.gob.ar/contribuyente/externo')
     re_login(browser, clave_fiscal)
-    # desplegable 
-    cuits = Select(browser.find_element(By.NAME,'$PropertySelection'))
-    total = {}
-    for i, cuit in enumerate(cuits.options):
+    # desplegable
+    try:
+      cuits = Select(browser.find_element(By.NAME,'$PropertySelection'))    
+      total = {}
+      for i, cuit in enumerate(cuits.options):
+        cant_imp, saldo = 0, 0
+        num_cuit = cuit.text
+        print(f'Leyendo info de CUIT: {num_cuit} \n')
+        total[num_cuit] = []
+        time.sleep(5)
+        cuits.select_by_index(i)
+        time.sleep(5)
+        conceptos = browser.find_elements(By.CSS_SELECTOR, 'tr[class="group"]')
+        print(f'Conceptos encontrados: {len(conceptos)} \n')
+        for concepto in conceptos:
+          elemento_cant_imp = concepto.find_element(By.CSS_SELECTOR, 'span[class="cant-impuesto"]').text
+          cant_imp += int(elemento_cant_imp[1:-1])
+          elemento_saldo = concepto.find_element(By.CSS_SELECTOR, 'td[class="subtotales sb_saldo"]').text
+          saldo += str_a_saldo(elemento_saldo)
+          elemento_int_r = concepto.find_element(By.CSS_SELECTOR, 'td[class="subtotales sb_int_res"]').text
+          saldo += str_a_saldo(elemento_int_r)
+          elemento_int_p = concepto.find_element(By.CSS_SELECTOR, 'td[class="subtotales sb_int_pun"]').text
+          saldo += str_a_saldo(elemento_int_p)
+        print(f'Cantidad de deuda total: {saldo}')   
+        print(f'Cantidad de obligaciones encontradas: {cant_imp} \n')
+        total.update({num_cuit: [cant_imp, saldo]})
+    except:
+      cuit = browser.find_element(By.CLASS_NAME, 'cuit')
+      total = {}
       cant_imp, saldo = 0, 0
       num_cuit = cuit.text
-      print(f'Leyendo info de CUIT: {num_cuit}')
+      print(f'Leyendo info de CUIT: {num_cuit} \n')
       total[num_cuit] = []
-      time.sleep(4)
-      cuits.select_by_index(i)
-      time.sleep(4)
+      time.sleep(5)
+      time.sleep(5)
       conceptos = browser.find_elements(By.CSS_SELECTOR, 'tr[class="group"]')
       print(f'Conceptos encontrados: {len(conceptos)}')
       for concepto in conceptos:
@@ -88,9 +105,8 @@ def deuda(browser, cuit, clave_fiscal):
         saldo += str_a_saldo(elemento_int_r)
         elemento_int_p = concepto.find_element(By.CSS_SELECTOR, 'td[class="subtotales sb_int_pun"]').text
         saldo += str_a_saldo(elemento_int_p)
-      print(f'Cantidad de deuda: {saldo}')   
-      print(f'Cantidad de obligaciones encontradas: {cant_imp}')
-       
+      print(f'Cantidad de deuda total: {saldo} \n')   
+      print(f'Cantidad de obligaciones encontradas: {cant_imp} \n')
       total.update({num_cuit: [cant_imp, saldo]})
     return total
   except:
@@ -119,38 +135,37 @@ def flattenlist(l):
   return flat_list          
 
 if __name__ == '__main__':
-  #extraccion de datos del excel
-  path = r"C:\Users\Administrator\Desktop\AFIP\afip\AFIP.xlsx"
-  wb_obj = openpyxl.load_workbook(path)  
-  sheet_obj = wb_obj.active
-  max_row = sheet_obj.max_row
+  #extraccion de datos de Google Sheets
+  credentials = '/credentials.json'
+  authorized_user = '/auth_user.json'
+  gc= gspread.oauth(credentials, authorized_user)
+  sh = gc.open("AFIP")
+  worksheet = sh.sheet1
+  max_row = len(worksheet.get_all_values()) +1
+  browser = webdriver.Chrome(options=options)
   #extrae cada usuario del excel
-  for i in range(2, max_row - 1):
-    cuit = extraer_cuil(i, sheet_obj, path)
-    clave_fiscal = extraer_clave(i, sheet_obj, path)
+  for i in range(2, max_row):
+    print(f"Trabajando con: {worksheet.cell(i, 1).value}") 
+    cuit = extraer_cuil(i, worksheet)
+    clave_fiscal = extraer_clave(i, worksheet)
     browser = webdriver.Chrome(options=options)
     login(browser, cuit, clave_fiscal)
     #A partir de aca solo realizar si el login fue exitoso
-    riesgo = siper(browser)
+    riesgo = str(siper(browser))
     #Escribir siper en excel
-    c1 = sheet_obj.cell(row = i, column = 4)
-    c1.value = riesgo
+    worksheet.update_cell(i, 4, riesgo)
     # deuda
     deuda_dic = deuda(browser, cuit, clave_fiscal)
     if type(deuda_dic) == dict:
       deuda_list = list(deuda_dic.values())
       deuda_list = flattenlist(deuda_list)
       for j in range(len(deuda_list)):
-        c1 = sheet_obj.cell(row = i, column = 7+j)
-        c1.value = deuda_list[j]
+        worksheet.update_cell(i, 7+j, deuda_list[j])
     else: 
-      print("Error al encontrar elemento deuda")    
+      print("Error al encontrar elemento deuda. \n")    
     # e-Servicios SRT
     notificaciones_personales = e_servicios_personal(browser)
-    c1 = sheet_obj.cell(row = i, column = 5)
-    c1.value = notificaciones_personales
+    worksheet.update_cell(i, 5, notificaciones_personales)
     notificaciones_juridicas = e_servicios_juridicos(browser)
-    c1 = sheet_obj.cell(row = i, column = 6)
-    c1.value = notificaciones_juridicas
-    wb_obj.save(path)
+    worksheet.update_cell(i, 6, notificaciones_juridicas)
   browser.close()  
